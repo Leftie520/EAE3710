f9d10709ca4e7eaba27db952e3431309d1823921
DeathWolf998 <sprad.jake@gmail.com>
2025-04-02 19:02:18 -0600

    Beginning to make the clone bumper

---
+++ Assets/Scripts/CloneBumper.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CloneBumper : Bumper
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D col)
+    {
+        base.OnCollisionEnter2D(col);
+        score += 100;
+        Debug.Log(score);
+        if()
+
+    }
+}
